7158f65a2f376ec357433fa8f29742d0bff436ab
Wallyson Galvão <wallyson.galvao@gmail.com>
2021-01-26 20:35:22 -0300

    inital commit

---
+++ ios/Pods/Target Support Files/Pods-inflationCalc-inflationCalcTests/Pods-inflationCalc-inflationCalcTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_inflationCalc_inflationCalcTests : NSObject
+@end
+@implementation PodsDummy_Pods_inflationCalc_inflationCalcTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-inflationCalc/Pods-inflationCalc-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_inflationCalc : NSObject
+@end
+@implementation PodsDummy_Pods_inflationCalc
+@end
